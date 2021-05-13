11de485b1fa2c2c032cf52597ab5106d140ffdef
neil245 <indranilghoshal5@gmail.com>
2021-05-14 00:51:26 +0530

    New file added to UC2

--- FirstAssignment.java
+++ FirstAssignment.java
@@ -3,6 +3,9 @@ package com.FirstAssignment;
 public class FirstAssignment {
 	public static void main(String[] args) {
 		//int attendance;
+		
+		 final int sal = 20;
+		 final int hours = 8;
 		System.out.println("Welcome");
 		int attendance = (int)Math.floor(Math.random()*2); 
 		System.out.println(attendance);
@@ -11,7 +14,11 @@ public class FirstAssignment {
 		}
 		else {
 			System.out.println("Employee is present");
+			int total_salary = sal*hours;
+					System.out.println(total_salary);
 		}
+		
+		
 	}
 
 }
