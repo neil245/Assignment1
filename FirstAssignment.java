4ce0600ce5b176dc222720a493c8a363dae43cae
neil245 <indranilghoshal5@gmail.com>
2021-05-14 00:42:01 +0530

    Files added

--- FirstAssignment.java
+++ FirstAssignment.java
@@ -2,7 +2,16 @@ package com.FirstAssignment;
 
 public class FirstAssignment {
 	public static void main(String[] args) {
+		//int attendance;
 		System.out.println("Welcome");
+		int attendance = (int)Math.floor(Math.random()*2); 
+		System.out.println(attendance);
+		if(attendance == 0) {
+			System.out.println("Employee is absent");
+		}
+		else {
+			System.out.println("Employee is present");
+		}
 	}
 
 }
